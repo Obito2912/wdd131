0d259a8c7e72ea833ce1e6eaab459c391077fcdc
Obito2912 <ovinsonlugo12@gmail.com>
2024-11-16 19:37:22 -0700

    Finish temple album website assignment for the week

---
+++ scripts/temples.js
@@ -0,0 +1,39 @@
+const currentYear = document.querySelector('#currentyear');
+const lastModified = document.querySelector('#lastModified');
+const headerMenu = document.querySelector('.header__menu');
+const navEl = document.querySelector('.header__nav');
+
+const today = new Date();
+const templesLarge = window.matchMedia('(min-width: 350px)');
+
+currentYear.textContent += today.getFullYear();
+lastModified.textContent += document.lastModified;
+
+function hideHambBtn(event) {
+    if (event.matches) {
+        headerMenu.classList.add('js-hide-content');
+    } else {
+        headerMenu.classList.remove('js-hide-content');
+    }
+}
+
+function showNavBarOnBiggerScreen(event) {
+    if (event.matches) {
+        navEl.classList.add('js-show-header-nav');
+    } else {
+        navEl.classList.remove('js-show-header-nav')
+    }
+}
+
+function handleClickOnMenu() {
+    headerMenu.classList.toggle('show');
+    navEl.classList.toggle('js-show-header-nav');
+}
+
+
+headerMenu.addEventListener('click', handleClickOnMenu);
+templesLarge.addEventListener('change', showNavBarOnBiggerScreen)
+templesLarge.addEventListener('change', hideHambBtn);
+
+showNavBarOnBiggerScreen(templesLarge);
+hideHambBtn(templesLarge);
