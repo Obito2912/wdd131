ca2b98f4446835dd26d80d6dfe570c1aa59a10b9
Obito2912 <https://obito2912.github.io/se_project_aroundtheus/>
2024-11-22 11:19:35 -0700

    Add formula for windChill

--- scripts/place.js
+++ scripts/place.js
@@ -1,8 +1,28 @@
-const currentYear = document.querySelector('#currentyear');
-const lastModified = document.querySelector('#lastModified');
 const today = new Date();
-const windChill = document.querySelector('#wind-chill');
+const currentYear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+const temperature = document.querySelector("#temperature");
+const wind = document.querySelector("#wind");
+const windChill = document.querySelector("#windChill");
+const windSpeedValue = 17;
+const temperatureValue = 61;
 
 currentYear.textContent += today.getFullYear();
 lastModified.textContent += document.lastModified;
-windChill.textContent = 'N/A';
+temperature.textContent = `${temperatureValue} °F`;
+wind.textContent = `${windSpeedValue} mph`;
+windChill.textContent = calculateWindChill(temperatureValue, windSpeedValue);
+
+function calculateWindChill(temperature, windSpeed) {
+  if (temperature > 50 || windSpeed < 3) {
+    return "N/A";
+  }
+
+  const windChill =
+    35.74 +
+    0.6215 * temperature -
+    35.75 * Math.pow(windSpeed, 0.16) +
+    0.4275 * temperature * Math.pow(windSpeed, 0.16);
+
+  return `${windChill.toFixed(2)} °F`;
+}
