025a0a8ba861e935f4c5527909dc8114aa3960f1
Obito2912 <https://obito2912.github.io/se_project_aroundtheus/>
2024-12-15 14:16:53 -0700

    Add arrow to menu to make easier for users to see it's a button

--- week06-07/scripts/theloveofnailz.js
+++ week06-07/scripts/theloveofnailz.js
@@ -1,4 +1,7 @@
 const menuBtn = document.querySelector(".header__menu");
 const navBar = document.querySelector(".header__nav");
 
-menuBtn.addEventListener("click", () => navBar.classList.toggle("open-popup"));
+menuBtn.addEventListener("click", () => {
+  navBar.classList.toggle("open-popup");
+  menuBtn.classList.toggle("header__menu_open");
+});
