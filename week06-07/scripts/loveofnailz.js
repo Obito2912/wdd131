61ed5f83f847476eadbfaacf26ad197f8e7d7d71
Obito2912 <ovinsonlugo12@gmail.com>
2024-12-15 17:15:23 -0700

    Change the look of the header menu

--- week06-07/scripts/loveofnailz.js
+++ week06-07/scripts/loveofnailz.js
@@ -1,7 +1,15 @@
+// const priceList = [
+//   {
+//     'full set acrylic, full set polly gel, 2-4 week fill, 5+ week fill, acrylic toes, 2-4 week acrylic toe fill, 5-6 week acrylic toe fill, gel polish toes only, gel polish manicure, luxury pedicure'
+//     headeradd - on: 'nail repair, medium art, simple art, detailed art, soak off/gel removal, free style art, add long-XL',
+//     cost: 4,
+//     eta: '',
+//   }
+// ]
+
 const menuBtn = document.querySelector(".header__menu");
 const navBar = document.querySelector(".header__nav");
 
 menuBtn.addEventListener("click", () => {
   navBar.classList.toggle("open-popup");
-  menuBtn.classList.toggle("header__menu_open");
 });
